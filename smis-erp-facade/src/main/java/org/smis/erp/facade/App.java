d30d2328a0d8fe48d797f2b3dd95ddc58191b140
dell <dell@windows10.microdone.cn>
2017-04-10 17:09:15 +0800

    ERP系统搭建

---
+++ smis-erp-facade/src/main/java/org/smis/erp/facade/App.java
@@ -0,0 +1,13 @@
+package org.smis.erp.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
